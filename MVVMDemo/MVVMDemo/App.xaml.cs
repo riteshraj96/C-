7b3b6c0855550969309b6847a37ee496ddc4edac
riteshraj96 <42927463+riteshraj96@users.noreply.github.com>
2019-10-24 12:24:40 +0530

    Add files via upload

---
+++ MVVMDemo/MVVMDemo/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace MVVMDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    base.OnStartup(e);
+        //    var mainWindow = new Window1();
+        //    var viewModel = new MainViewModel();
+        //    mainWindow.DataContext = viewModel;
+        //    mainWindow.Show();
+        //}
+    }
+}
